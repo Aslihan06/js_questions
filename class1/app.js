cacf75c90a1a93fd9af7f9a62558580abae663f5
Aslıhan Esen <aslyqc14@gmail.com>
2023-04-06 04:27:01 +0300

    questions6

--- class1/app.js
+++ class1/app.js
@@ -103,5 +103,17 @@ if (not>100 || not<0){
 }
 
 
+//?6- Artık yıl sorusu.Artık yıl, bir yıldaki günlerin 365 yerine 366 gün olmasına denir. Bu fazladan gün (artık gün), normalde 28 gün olan şubat ayına 29 Şubat’ın eklenmesi ile elde edilir.  Bunun hesaplanması için aşağıdaki algoritma kullanılır.
+//!Bir tarih 4 ile tam bölünüp 100 katları ile tam bölünemiyorsa, ve yıl 400 sayısına tam bölünüyorsa artık yıldır. BU iki şartın dışında yılarda şubat ayı 28 gün, artık yıllarda 29 gün olarak belirlenir.
+
+//? Yukarıdaki bilgilere göre bir yılın artık yıl olup olmadığını konsola yazdıran conditional statement'i yazınız.
+
+const yil = 2013;
+
+if(yil%4===0 || yil%100 !==0 && yil%400===0){
+    console.log(`${yil} artık yıldır`);
+}else{
+    console.log(`${yil} artık yıl değildir`);
+}
 
 
