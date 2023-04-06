aadb67b627e8ed08f3c207aae343811f82b55a14
Aslıhan Esen <aslyqc14@gmail.com>
2023-04-06 03:51:43 +0300

    questions4

--- class1/app.js
+++ class1/app.js
@@ -25,3 +25,45 @@ const age = 18;
 age < 18 || age >= 18
 ?console.log(`Oy kullanabilirsiniz.`)
 :console.log(`Oy kullanamazsınız.`);
+
+//? 4- Haftanın günlerinin numaraları aşağıda belirtilmiştir. Buna göre belirtilen bir sayının hangi güne karşılık gelidiğini switch case yapı ile konsola yazdıran kodu yazınız.
+
+// pazar 0
+// Pazartesi 1
+// Salı 2
+// Çarşamba 3
+// Perşembe 4
+// Cuma 5
+// Cumartesi 6
+
+const day = 8;
+
+switch (day) {
+    case 0:
+        console.log(`Pazar`);
+        
+        break;
+    case 1:
+        console.log(`Pazartesi`);
+        break;
+    case 2:
+        console.log(`Salı`);
+        break;
+    case 3:
+        console.log(`Çarşamba`);
+        break;
+    case 4:
+        console.log(`Perşembe`);
+        break;
+    case 5:
+        console.log(`Cuma`);
+        break;
+    case 6:
+        console.log(`Cumartesi`);
+        break;            
+
+
+    default:
+        console.log(`Lütfen günlere denk gelen numarayı giriniz.`);
+        break;
+}
