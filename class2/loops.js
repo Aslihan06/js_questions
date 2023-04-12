39a80f0baebf5dce164b166d6d9bb3bd8e80b17d
Aslıhan Esen <aslyqc14@gmail.com>
2023-04-12 14:15:39 +0300

    questions 2

--- class2/loops.js
+++ class2/loops.js
@@ -1,8 +1,21 @@
 //! 1) 1000 İLE 2000 ARASINDAKİ 13'E BÖLÜMÜNDEN 3 KALAN SAYILARI CONSOLA YAZDIRINIZ
 
 
-for(i=1000;i<2000;i++){
-    if(i % 13 == 3){
-        console.log(i);
-    }
-}
+// for(let i=1000;i<2000;i++){
+//     if(i % 13 == 3){
+//         console.log(i);
+//     }
+// }
+
+//! 2) Kullanıcıdan alınan 5 sayısının ortalamasını consola yazdırınız.
+
+// let sayac=0;
+// let sum = 0;
+// while(sayac<5){
+//     const sayi = +prompt("bir sayı giriniz")
+
+
+//     sum+=sayi
+//     sayac++
+// }
+// console.log(sum/5, sayac);
